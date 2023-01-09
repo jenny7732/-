553b29f127150310fc5c57ec1d001a1192498b4a
jenny7732 <jenny7732@naver.com>
2023-01-09 14:39:57 +0900

    짝수와 홀수 더 좋은 코드 추가

--- step1/짝수와 홀수.js
+++ step1/짝수와 홀수.js
@@ -2,3 +2,8 @@ function solution(num) {
   var answer = (num%2==1 || num%2 ==-1) ? "Odd" : "Even";
   return answer; 
 }
+
+// num % 2 의 결과가 0이면 false!!
+function evenOrOdd(num) {
+  return num % 2 ? "Odd" : "Even";
+}
